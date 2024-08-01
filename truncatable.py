abd0b53593fae2570c40968f5984f66d3706d81c
Eduardo Medina <p.eduardomego@gmail.com>
2024-07-31 20:59:11 -0600

    Commit

--- truncatable.py
+++ truncatable.py
@@ -1,29 +1,81 @@
 def truncatable(n):
+    ns = str(n)
+    for i in ns:
+        if i=='0':
+            print('false')
+            return 0
+    Flag = []
+    Flag.append(LeftTruncatable(n))
+    Flag.append(RightTruncatable(n))
+    if Flag[0] and Flag[1]:
+        print('both')
+    else:
+        if Flag[0]:
+            print('left')
+        elif Flag[1]:
+            print('right')
+        else:
+            print('false')        
     return 0
 
+def LeftTruncatable(n):
+    sn = str(n)
+    for i in range(len(sn)):
+        num = int(sn[i:])
+        factors = prime_factors(num)
+        if len(factors)!=1:return False
+    return True
+
+def RightTruncatable(n):
+    sn = str(n)
+    for i in range(len(sn)-1,-1,-1):
+        num = int(sn[0:i+1])
+        factors = prime_factors(num)
+        if len(factors)!=1:return False
+    return True
+    
+    
+
+def prime_factors(n):
+    i = 2
+    factors = {}
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            if i in factors.keys():
+                factors[i] += 1
+            else:
+                factors[i] = 1
+    if n > 1:
+        if n in factors.keys():
+            factors[n] +=1 
+        else:
+            factors[n] = 1
+    return factors
 
 
-print(truncatable(9137))
+truncatable(9137)
 # "left"
 # Because 9137, 137, 37 and 7 are all prime.
-"""
-print(truncatable(5939))
+
+truncatable(5939)
 # "right"
 # Because 5939, 593, 59 and 5 are all prime.
 
-print(truncatable(317))
+truncatable(317)
 # "both"
 # Because 317, 17 and 7 are all prime and 317, 31 and 3 are all prime.
 
-print(truncatable(5))
+truncatable(5)
 # "both"
 # The trivial case of single-digit primes is treated as truncatable from both directions.
 
-print(truncatable(139))
+truncatable(139)
 # False
 # 1 and 9 are non-prime, so 139 cannot be truncatable from either direction.
 
-print(truncatable(103))
+truncatable(103)
 # False
 # Because it contains a 0 digit (even though 103 and 3 are primes).
-"""
